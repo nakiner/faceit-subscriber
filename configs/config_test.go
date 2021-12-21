e2048636e933d8ee22792281174c88a40d04cf49
Pavel Emelyanov <p.emelyanov@agimagroup.ru>
2021-12-21 23:15:08 +0300

    fix test after copy

--- configs/config_test.go
+++ configs/config_test.go
@@ -12,11 +12,11 @@ func TestNewConfig(t *testing.T) {
 }
 
 func TestConfig_Read(t *testing.T) {
-	port := 9154
+	port := 4222
 	exp := &Config{}
-	exp.Metrics.Port = port
-	os.Setenv("FACEIT_METRICS_PORT", fmt.Sprintf("%d", port))
+	exp.Nats.Port = port
+	os.Setenv("FACEIT_SUBSCRIBER_NATS_PORT", fmt.Sprintf("%d", port))
 	c := NewConfig()
 	c.Read()
-	assert.Equal(t, exp.Metrics.Port, c.Metrics.Port)
+	assert.Equal(t, exp.Nats.Port, c.Nats.Port)
 }
